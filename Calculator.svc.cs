96cc8e982de864352560db5cc5e29c86beea19d7
HamzaAnis <hamzaanis9514@gmail.com>
2017-04-20 12:55:12 +0500

    Initial commit

---
+++ Calculator.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFLABWEB
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calculator" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Calculator.svc or Calculator.svc.cs at the Solution Explorer and start debugging.
+    public class Calculator : ICalculator
+    {
+
+        public void DoWork()
+        {
+        }
+
+        public int add(int n1, int n2)
+        {
+            return n1 + n2;
+            throw new NotImplementedException();
+        }
+
+        public int sub(int n1, int n2)
+        {
+            return n1 - n2;
+            throw new NotImplementedException();
+        }
+
+        public int div(int n1, int n2)
+        {
+            return n1/n2;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ICalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFLABWEB
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculator" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        int add(int n1,int n2);
+
+        [OperationContract]
+        int sub(int n1,int n2);
+
+
+        [OperationContract]
+        int div(int n1, int n2);
+
+    }
+}
